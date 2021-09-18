8a87dddd8b2f3afa3f08bc2f72209ec5d1c1455e
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-18 15:35:19 +0600

    search feature added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
+// error section
+
+const error = document.getElementById("error");
+error.style.display = "none";
+
 // loading spinner
 
 const loadingSpinner = (show) => {
@@ -8,12 +13,37 @@ const loadingSpinner = (show) => {
   }
 };
 
+// search drinks
+
+document
+  .getElementById("drinks__search__button")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    const input = document.getElementById("drinks__search__input");
+    const inputValue = input.value;
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`;
+    try {
+      error.style.display = "none";
+      const res = await fetch(url);
+      const data = await res.json();
+      displayDrinks(data.drinks);
+    } catch {
+      error.innerText = "Please Search a valid Drinks";
+      error.style.color = "red";
+      error.style.textAlign = "center";
+      error.style.fontSize = "30px";
+      error.style.fontWeight = 800;
+      error.style.display = "block";
+    }
+    input.value = "";
+  });
+
 // fetch non alcoholic data
 
 const fetchData = async () => {
   const url =
     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
-   loadingSpinner(true)
+  loadingSpinner(true);
   const res = await fetch(url);
   const data = await res.json();
   displayDrinks(data.drinks);
@@ -27,6 +57,7 @@ fetchData();
 
 const displayDrinks = (drinks) => {
   const drinksContainer = document.getElementById("drinks__container");
+  drinksContainer.textContent = "";
   drinks.forEach((drink) => {
     const { strDrink, strDrinkThumb, idDrink } = drink;
     const drinksCard = document.createElement("div");
@@ -36,9 +67,9 @@ const displayDrinks = (drinks) => {
         <h3>${strDrink}</h3>
     `;
     drinksContainer.appendChild(drinksCard);
-    loadingSpinner(false)
+    loadingSpinner(false);
     drinksCard.addEventListener("click", async () => {
-     loadingSpinner(true)
+      loadingSpinner(true);
       const detailsUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
       const res = await fetch(detailsUrl);
       const data = await res.json();
@@ -93,18 +124,7 @@ const displayDetails = (drink) => {
           <p>${strInstructions}</p>    
           <h4>Glass: ${strGlass}</h4>
           <h4>Ingredients:</h4>
-          <ul>
-            <li>${strMeasure1 ? strMeasure1 : ""} ${strIngredient1 ? strIngredient1 : ""}</li>
-            <li>${strMeasure2 ? strMeasure2 : ""} ${strIngredient2 ? strIngredient2 : ""}</li>
-            <li>${strMeasure3 ? strMeasure3 : ""} ${strIngredient3 ? strIngredient3 : ""}</li>
-            <li>${strMeasure4 ? strMeasure4 : ""} ${strIngredient4 ? strIngredient4 : ""}</li>
-            <li>${strMeasure5 ? strMeasure5 : ""} ${strIngredient5 ? strIngredient5 : ""}</li>
-            <li>${strMeasure6 ? strMeasure6 : ""} ${strIngredient6 ? strIngredient6 : ""}</li>
-            <li>${strMeasure7 ? strMeasure7 : ""} ${strIngredient7 ? strIngredient7 : ""}</li>
-            <li>${strMeasure8 ? strMeasure8 : ""} ${strIngredient8 ? strIngredient8 : ""}</li>
-            <li>${strMeasure9 ? strMeasure9 : ""} ${strIngredient9 ? strIngredient9 : ""}</li>
-            <li>${strMeasure10 ? strMeasure10 : ""} ${strIngredient10 ? strIngredient10 : ""}</li>
-          </ul>
+         
 
           <p class="copyright">&copy; Copyright All Right Reserved By
             <a href="https://github.com/farhan-nahid/" target="_blank">Farhan</a>
@@ -113,7 +133,7 @@ const displayDetails = (drink) => {
   `;
 
   modalContainer.appendChild(modal);
-  loadingSpinner(false)
+  loadingSpinner(false);
 
   // hide modal
   document.getElementById("cross__icon").addEventListener("click", () => {
@@ -121,11 +141,23 @@ const displayDetails = (drink) => {
     modalContainer.textContent = "";
   });
 
-  const lis = document.getElementsByTagName('li')
-    for(const li of lis){
-       if(li.innerText === ''){
-        li.style.display = 'none'
-       }
+  const lis = document.getElementsByTagName("li");
+  for (const li of lis) {
+    if (li.innerText === "") {
+      li.style.display = "none";
     }
+  }
 };
-
+/* 
+<ul>
+<li>${strMeasure1 ? strMeasure1 : ""} ${strIngredient1 ? strIngredient1 : ""}</li>
+<li>${strMeasure2 ? strMeasure2 : ""} ${strIngredient2 ? strIngredient2 : ""}</li>
+<li>${strMeasure3 ? strMeasure3 : ""} ${strIngredient3 ? strIngredient3 : ""}</li>
+<li>${strMeasure4 ? strMeasure4 : ""} ${strIngredient4 ? strIngredient4 : ""}</li>
+<li>${strMeasure5 ? strMeasure5 : ""} ${strIngredient5 ? strIngredient5 : ""}</li>
+<li>${strMeasure6 ? strMeasure6 : ""} ${strIngredient6 ? strIngredient6 : ""}</li>
+<li>${strMeasure7 ? strMeasure7 : ""} ${strIngredient7 ? strIngredient7 : ""}</li>
+<li>${strMeasure8 ? strMeasure8 : ""} ${strIngredient8 ? strIngredient8 : ""}</li>
+<li>${strMeasure9 ? strMeasure9 : ""} ${strIngredient9 ? strIngredient9 : ""}</li>
+<li>${strMeasure10 ? strMeasure10 : ""} ${strIngredient10 ? strIngredient10 : ""}</li>
+</ul> */
